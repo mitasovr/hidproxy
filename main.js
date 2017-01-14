875397cc56f3dea97f3c424fed2e5dc2aae19491
Roman Mitasov <roman.mitasov@techops.ru>
2017-01-14 16:10:24 +0300

    upd

--- main.js
+++ main.js
@@ -2,8 +2,16 @@
 
 
 var stdin = process.openStdin();
-require('tty').setRawMode(true);
+stdin.setRawMode(true);
+stdin.resume();
 
+stdin.on('data', function (key) {
+    process.stdout.write('on data: ' + key);
+
+    if (key === '\u0003') {
+        process.exit();
+    }
+});
 stdin.on('keypress', function (chunk, key) {
     process.stdout.write('Get Chunk: ' + chunk + '\n');
     if (key && key.ctrl && key.name == 'c') process.exit();
