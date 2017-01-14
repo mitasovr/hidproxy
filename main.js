426ab95805b1b27ba973fa9613d6e90c8e760ab6
Roman Mitasov <roman.mitasov@techops.ru>
2017-01-14 16:04:07 +0300

    upd

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-var mouse = require('./mouse.js');
+// var mouse = require('./mouse.js');
 
-console.log(mouse);
+
+var stdin = process.openStdin();
+require('tty').setRawMode(true);
+
+stdin.on('keypress', function (chunk, key) {
+    process.stdout.write('Get Chunk: ' + chunk + '\n');
+    if (key && key.ctrl && key.name == 'c') process.exit();
+});
+// console.log(mouse);
