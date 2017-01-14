40182656f248c2321ebb6a1ab0891837c8aacf4b
Roman Mitasov <roman.mitasov@techops.ru>
2017-01-14 16:22:04 +0300

    upd

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
+var HID = require('node-hid');
+
+var devices = HID.devices();
+console.log(devices);
+
+
 // var mouse = require('./mouse.js');
 
 
-var stdin = process.openStdin();
-stdin.setRawMode(true);
-stdin.resume();
+// var stdin = process.openStdin();
+// stdin.setRawMode(true);
+// stdin.resume();
 
-stdin.on('data', function (key) {
-    process.stdout.write('on data: ' + key);
+// stdin.on('data', function (key) {
+//     process.stdout.write('on data: ' + key);
 
-    if (key === '\u0003') {
-        process.exit();
-    }
-});
-stdin.on('keypress', function (chunk, key) {
-    process.stdout.write('Get Chunk: ' + chunk + '\n');
-    if (key && key.ctrl && key.name == 'c') process.exit();
-});
+//     if (key === '\u0003') {
+//         process.exit();
+//     }
+// });
+// stdin.on('keypress', function (chunk, key) {
+//     process.stdout.write('Get Chunk: ' + chunk + '\n');
+//     if (key && key.ctrl && key.name == 'c') process.exit();
+// });
 // console.log(mouse);
